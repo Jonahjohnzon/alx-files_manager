90a648b7d39f90cff0cf8e039a1cbeaac1adae7d
jonah ominyi <jonahjohnzon@gmail.com>
2024-06-19 14:36:09 +0000

    push

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,24 +2,20 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static getStatus(req, res) {
-    try {
-      const redis = redisClient.isAlive();
-      const dab = dbClient.isAlive();
-      res.status(200).send({ redis, dab });
-    } catch (err) {
-      console.log(err);
-    }
+  static getStatus(request, response) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    response.status(200).send(status);
   }
 
-  static async getStats(req, res) {
-    try {
-      const users = await dbClient.nbUsers();
-      const files = await dbClient.nbFiles();
-      res.status(200).send({ users, files });
-    } catch (err) {
-      console.log(err);
-    }
+  static async getStats(request, response) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    response.status(200).send(stats);
   }
 }
 
