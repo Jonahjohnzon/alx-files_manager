fdba270dd964177ca11894fd42dd3f9c34eb40cf
jonah ominyi <jonahjohnzon@gmail.com>
2024-06-19 15:20:38 +0000

    push

---
+++ utils/basic.js
@@ -0,0 +1,19 @@
+import { ObjectId } from 'mongodb';
+
+const basicUtils = {
+  /**
+   * Checks if Id is Valid for Mongo
+   * @id {string|number} id to be evaluated
+   * @return {boolean} true if valid, false if not
+   */
+  isValidId(id) {
+    try {
+      ObjectId(id);
+    } catch (err) {
+      return false;
+    }
+    return true;
+  },
+};
+
+export default basicUtils;
